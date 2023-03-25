c17cfe988d9a398f9f32db5330cf56a5eb69130e
Praduman Prajapati <tech.praduman@gmail.com>
2023-03-25 19:05:52 +0530

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,135 @@
+import java.util.Scanner;
+public class loops {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+//       while loops
+//        printing 100 times via while loop
+//        int a = 0;
+//        while (a<100){
+//            System.out.println("hello world");
+//            a++;
+//        }
+
+        // printing 1 to 10 via while loop
+//        int count = 1;
+//        while(count<=10){
+//            System.out.println(count);
+//            count++;
+//        }
+
+        //printing 1 to n via while loop
+//        int d= 1;
+//        System.out.println("how many times you want to print ?");
+//        int u= sc.nextInt();
+//        while (d<=u){
+//            System.out.println(d);
+//            d++;
+//        }
+
+
+
+        //printing sum of n numbers
+//        System.out.println("enter no. jaha tk sum print krna ho");
+//        int i= 1;
+//        int sum = 0;
+//        int j= sc.nextInt();
+//        while(i<=j){
+//            sum+=i;
+//            i++;
+//        }
+//        System.out.println("sum is " + sum);
+
+        // for loop
+        // printing n times via for loop
+//        System.out.println("how many times you want to print ?");
+//        int r=sc.nextInt();
+//        for(int y=1; y<=r; y++){
+//            System.out.println("hello world");
+//        }
+
+        //print square pattern
+//        int i = 1;
+//        while(i<=4){
+//                System.out.println("****");
+//            i++;
+//        }
+
+
+      //  print reverse of a number
+//        System.out.println("enter no. to reverse");
+//        long s= sc.nextInt();
+//        while (s>0){
+//            long lastdigit =s%10;
+//            System.out.print(lastdigit);
+//            s = s/10;
+//        }
+
+        //  reverse the given no.
+//        System.out.println("enter no. to reverse");
+//        int s= sc.nextInt();
+//        int reverse =0;
+//        while(s>0){
+//            int last = s%10;
+//            s=s/10;
+//            reverse = reverse*10 + last;
+//        }
+//        System.out.println("reverse is  "+ reverse);
+
+
+        //do while loop
+//        int f =1;
+//        do{
+//            System.out.println("hello world");
+//            f++;
+//        }while (f<=10);
+
+
+        //break statements
+//        for( int l =0; l<=10; l++){
+//            if(l==6){
+//                System.out.println("you are out of the loop");
+//                break;
+//            }
+//            System.out.println("bhag bsdk");
+//        }
+
+//continue statement
+//        for(int i=1;i<=5;i++){
+//            if(i==4){
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+
+        //printing with skip using continue
+//        do{
+//            System.out.println("enter your no. ");
+//            int n =sc.nextInt();
+//            if(n%10 ==0){
+//                continue;
+//            }
+//            System.out.println("number was: "+ n);
+//        }while (true);
+
+        //no. is prime or not
+//        System.out.println("enter no. to check whether it is prime or not");
+//        int n = sc.nextInt();
+//        int cou =0;
+//        for(int u=2;u<n;u++){
+//            if (n%u==0){
+//                cou ++;
+//            }
+//            else {
+//                cou=cou;
+//            }
+//        }
+//        if (cou>0){
+//            System.out.println("not prime");
+//        }
+//     else {
+//            System.out.println("prime");
+//        }
+        }
+}
