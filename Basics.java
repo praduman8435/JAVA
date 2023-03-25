e0388bcc8a104dc2aa710de58c7d3698d197aec0
Praduman Prajapati <tech.praduman@gmail.com>
2023-03-25 19:04:31 +0530

    Create Basics.java

---
+++ Basics.java
@@ -0,0 +1,101 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+import java.util.*;
+public class javabasics {
+    public static void main(String args[]) {
+
+//print hello world
+        System.out.println("Hello World");
+//print pattern
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+
+//find perimeter of rectangle
+        int a=10;
+        int b=5;
+        System.out.println("perimeter of rectangle-->"+(2*(a+b)));
+
+
+
+        byte f= 8;   //1byte
+        System.out.println(f);
+        char ch = 'a';   //2byte
+        System.out.println(ch);
+        boolean var =false;  //1byte
+        System.out.println(var);
+        double price= 10.3;    //8byte
+        System.out.println(price);
+        int u=32;  //4byte
+        System.out.println(u);
+        float j=4.3f;
+        System.out.println(j);
+        long h= 6373626;   //8byte
+        System.out.println(h);
+        short n=230;    //2byte
+        System.out.println(n);
+
+//taking input from the user
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.println("enter your first name ");
+//        String input = sc.next();
+//        System.out.println(input);
+
+//        System.out.println("full name ?");
+//        String name = sc.nextLine();
+//        System.out.println(name);
+
+//        System.out.println("number?");
+//        int number = sc.nextInt();
+//        System.out.println(number);
+
+//        System.out.println("number?");
+//        float jhh = sc.nextFloat();
+//        System.out.println(jhh);
+
+//        System.out.println("true or false");
+//        boolean gh= sc.nextBoolean();
+//        System.out.println(gh);
+
+//        System.out.println("a ?");
+//        int y = sc.nextInt();
+//        System.out.println("b ?");
+//        int t= sc.nextInt();
+//        System.out.println("sum is "+(y+t));
+
+//        System.out.println("a ?");
+//        int o = sc.nextInt();
+//        System.out.println("b ?");
+//        int p= sc.nextInt();
+//        System.out.println("a*p is "+(o*p));
+
+//        System.out.println("radius?");
+//        float w = sc.nextFloat();
+//        float area = 3.14f*w*w;
+//        System.out.println("area of circle is "+area);
+
+      //  type casting
+//        float r= 43.54f;
+//        int q= (int)r;
+//        System.out.println(q);
+
+//        char re ='a';
+//        int number=re;
+//        System.out.println(number);
+
+
+        //type promotion in expression
+//        char r ='a';
+//        char e ='b';
+//        System.out.println("b-a = "+ (e-r));
+
+//        byte w=5;
+//        b=(byte)(b*2);
+//        System.out.println(b);
+    }
+}
